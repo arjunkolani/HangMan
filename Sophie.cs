bfebbffcbabe13f04c0139d06c736931f598f6c5
sophie at school <sophie at school@SOPHIESBAE>
2023-10-04 14:16:13 +0100

    simplified gallows

--- Sophie.cs
+++ Sophie.cs
@@ -11,50 +11,9 @@ namespace HangMan
 
         public static string OutputGallows(int WrongTries)
         {
-            string Design = "";
+            string[] Design = new string[10] { "safe :)", " \n |\n |\n |\n |\n |\n |\n_|___", "_________\n |\n |\n |\n |\n |\n |\n_|___", "_________\n |/      |\n |\n |\n |\n |\n |\n_|___", "_________\n |/      |\n |      (_)\n |\n |\n |\n |\n_|___", "_________\n |/      |\n |      (_)\n |       |\n |       |\n |\n |\n_|___", "_________\n |/      |\n |      (_)\n |      \\|\n |       |\n |\n |\n_|___", "_________\n |/      |\n |      (_)\n |      \\|/\n |       |\n |\n |\n_|___", "_________\n |/      |\n |      (_)\n |      \\|/\n |       |\n |      / \n |\n_|___", "_________\n |/      |\n |      (_)\n |      \\|/\n |       |\n |      / \\\n |\n_|___" };
             // there will be 9 possible mistakes
-            if (WrongTries == 0)
-            {
-                Design = "safe :)";
-            }
-            else if (WrongTries == 1)
-            {
-                Design = " \n |\n |\n |\n |\n |\n |\n_|___";
-            }
-            else if (WrongTries == 2)
-            {
-                Design = "_________\n |\n |\n |\n |\n |\n |\n_|___";
-            }
-            else if (WrongTries == 3)
-            {
-                Design = "_________\n |/      |\n |\n |\n |\n |\n |\n_|___";
-            }
-            else if (WrongTries == 4)
-            {
-                Design = "_________\n |/      |\n |      (_)\n |\n |\n |\n |\n_|___";
-            }
-            else if (WrongTries == 5)
-            {
-                Design = "_________\n |/      |\n |      (_)\n |       |\n |       |\n |\n |\n_|___";
-            }
-            else if (WrongTries == 6)
-            {
-                Design = "_________\n |/      |\n |      (_)\n |      \\|\n |       |\n |\n |\n_|___";
-            }
-            else if (WrongTries == 7)
-            {
-                Design = "_________\n |/      |\n |      (_)\n |      \\|/\n |       |\n |\n |\n_|___";
-            }
-            else if (WrongTries == 8)
-            {
-                Design = "_________\n |/      |\n |      (_)\n |      \\|/\n |       |\n |      / \n |\n_|___";
-            }
-            else
-            {
-                Design = "_________\n |/      |\n |      (_)\n |      \\|/\n |       |\n |      / \\\n |\n_|___";
-            }
-            // full design = "_________\n |/      |\n |      (_)\n |      \\|/\n |       |\n |      / \\\n |\n_|___"
-            return Design;
+            return Design[WrongTries];
         }
 
         public static string DisplayGuesses(string word, char[] letters)
